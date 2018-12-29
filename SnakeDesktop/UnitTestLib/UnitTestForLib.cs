d32bc307cb09ff02c132852c183fd0586d001742
TheSylwek97 <38591912+TheSylwek97@users.noreply.github.com>
2018-12-29 19:27:37 +0100

    UnitTest

--- SnakeDesktop/UnitTestLib/UnitTestForLib.cs
+++ SnakeDesktop/UnitTestLib/UnitTestForLib.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ClassLibrary_Logic;
+using System.Collections.Generic;
+using System.Drawing;
 
 namespace UnitTestLib
 {
@@ -8,8 +10,22 @@ namespace UnitTestLib
     public class UnitTestForLib
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestMethodForSettings()
+        {
+            Console.WriteLine("Pokaż usawtenia domyślne:");
+            Console.WriteLine("Width " + Settings.Width);
+            Console.WriteLine("Height " + Settings.Height);
+            Console.WriteLine("Speed " + Settings.Speed);
+            Console.WriteLine("Score " + Settings.Score);
+            Console.WriteLine("Points " + Settings.Points);
+            Console.WriteLine("GameOver " + Settings.GameOver);
+            Console.WriteLine("direction " + Settings.direction);
+        }
+
+        [TestMethod]
+        public void TestMethodForCircle()
         {
         }
     }
 }
+
